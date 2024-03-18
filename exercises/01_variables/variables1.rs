bafb76d8d61aec6b30539ece669fa434f657bde7
Caleb Grant <geocoug@gmail.com>
2024-03-18 08:16:24 -0700

    complete exercise 1-5
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -5,9 +5,14 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+fn print_number(x: i32) {
+    println!("x is: {}", x);
+}
 
 fn main() {
-    x = 5;
+    let mut x = 5;
+    print_number(x);
+    x = 10;
+    print_number(x);
     println!("x has the value {}", x);
 }
