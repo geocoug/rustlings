dd6f8e92720a8803171c134b2f5ae089cb25a35f
Caleb Grant <geocoug@gmail.com>
2024-10-02 13:13:35 -0700

    complete exercises 6-8
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -44,6 +48,9 @@ impl State {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
         // fn function((t, u, p, l, e))
+        fn ChangeColor((u8, u8, u8)) {
+            
+        }
     }
 }
 
